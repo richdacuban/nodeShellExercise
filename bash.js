a2b2c943ce2ed4e589be6871e07d9d3e63287986
Richard Dominguez <rrjdominguez@gmail.com>
2018-09-18 12:46:46 -0400

    pwd working

--- bash.js
+++ bash.js
@@ -2,11 +2,4 @@ const pwd = require('./pwd');
 
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', data => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write('You typed: ' + cmd);
-    console.log(__dirname);
-    process.stdout.write('\nprompt > ');
-  }
-});
+pwd();
--- pwd.js
+++ pwd.js
@@ -2,7 +2,6 @@ module.exports = function() {
   process.stdin.on('data', data => {
     const cmd = data.toString().trim();
     if (cmd === 'pwd') {
-      process.stdout.write('You typed: ' + cmd);
       console.log(__dirname);
       process.stdout.write('\nprompt > ');
     }
